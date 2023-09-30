db3a68c6e01cd7d47118be834b8dcbfe9da69ff5
Micah Killian <micah@killianarts.online>
2023-09-30 07:02:09 -0500

    wip

--- winadmin/views.py
+++ winadmin/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import pytz
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
