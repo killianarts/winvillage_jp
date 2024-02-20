e72ed5499e642f1d96845e2c1b660800e9c5ae78
Micah Killian <micah@killianarts.online>
2024-02-20 10:27:41 +0900

    wip

--- winvillage/settings.py
+++ winvillage/settings.py
@@ -182,6 +182,7 @@ TEMPLATES = [
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
+                "core.context_processors.debug",
             ],
             "loaders": default_loaders if DEBUG else cached_loaders,
             "builtins": [
