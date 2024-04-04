0f048c7e714140b546110a15f9478a3035b8d5be
Micah Killian <micah@killianarts.online>
2024-04-04 10:56:57 +0900

    wip

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "core"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="homepage"),
     path("rooms/", views.room_details, name="room_details"),
     path("get-messages/", views.get_messages, name="get_messages"),
     path("flush-session/", views.flush_session, name="flush_session"),
